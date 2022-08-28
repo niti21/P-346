86efa0f05995db58bdbb3a472c6c6afdeb80a04a
Niti <81793990+niti21@users.noreply.github.com>
2022-08-28 23:59:12 +0530

    Add files via upload

---
+++ Assignment 2/lcglib.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+import matplotlib.pyplot as plot
+a = 1103515245
+c = 12345
+m = 32768.
+x = 10
+def lcg(a,c,m,x):
+    y = ((a*x + c) % m)/m
+    return y
+def lcgplot(a,c,m,x):
+    rnum = []
+    for i in range(0,500):
+        y = (a*x + c) % m
+        rnum.append(y)
+        x=y
+        i= i+1  
+        plt=plot.scatter(i, y) 
+    return plt
+def display(x):
+    print(lcg(1103515245, 12345, 32768, x))
+    lcgplot(1103515245, 12345, 32768, x)
+        
+
