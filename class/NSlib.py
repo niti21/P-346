a6bc4102a4fea304ddc5d25bc742af7b3bf0f1d7
Niti <81793990+niti21@users.noreply.github.com>
2022-10-20 00:36:21 +0530

    Update NSlib.py

--- class/NSlib.py
+++ class/NSlib.py
@@ -5,7 +5,7 @@
 
 
 get_ipython().run_line_magic('matplotlib', 'inline')
-
+import math
 import matplotlib.pyplot as plot
 a = 1103515245
 c = 12345
@@ -308,10 +308,6 @@ def jacobi(a,b,it,tol):
 
 
 # In[5]:
-
-
-import math
-
 #Defining functions
 def f(q):
     y = (q**4) - (q**3)-7*(q**2) + q + 6
@@ -428,6 +424,91 @@ def laguerreMethod(x0, n, e):
     print("No. of iterations:", t)
     return xk
 
+#Alternative
+def deflation(c,a):
+    if len(c) != 1:
+        c[1] = c[1] + c[0]*a
+        for i in range(2,len(c)):
+            c[i] = c[i] + a*c[i - 1]
+        c.pop()
+    else:
+        print("cannot deflate")
+    return c
+#########################################
+def copy_list(c):
+    cn = []
+    for i in range(0,len(c)):
+        a = c[i]
+        cn.append(a)
+    return cn
+#######################################
+def derivative(z):
+   
+    for i in range(0,len(z)):
+        z[i] = z[i]*(len(z)-i-1)
+    z.pop()
+   
+    return z
+
+#########################################
+def fn(c,z):
+    sum1 =0
+    for i in range(0,len(c)):
+        sum1 = sum1 + c[i]*(z**(len(c)-i-1))
+    return sum1
+###########################################
+def laguerre_2(c,z,degree):
+    t = 0
+    f = 7
+     
+    k1 = copy_list(c)
+    k2 = copy_list(c)
+    k2 = derivative(k2)
+   
+    while t < degree:
+        if abs(fn(c,z))<0.0000001:
+            print(fn(c,z))
+            print(z, "is a root")
+        else:
+            k1 = derivative(k1)
+            k2 = derivative(k2)
+            # print(c)
+            # print(k1)
+            # print(k2)
+            count = 0
+            while abs(fn(c,z))>0.0000001:
+                f = z
+               
+                sum1 = 0
+                for i in range(0,len(k1)):
+                    sum1 = sum1 + k1[i]*(z**(len(k1)-i-1))
+                G = sum1/fn(c,z)
+               
+                sum2 = 0
+                for i in range(0,len(k2)):
+                    sum2 = sum2 + k2[i]*(z**(len(k2)-1-i))
+                H = G**2 - (sum2/fn(c,z))
+               
+                n = len(c)-1
+                if G < 0:
+                    a = n/(G - ((n-1)*(n*H - G**2))**0.5)
+                else:
+                    a = n/(G + ((n-1)*(n*H - G**2))**0.5)
+                z = z - a
+                count = count + 1
+        print(z,"is a root")
+        # print(count,'is the number of iterations used to get',z)
+        c = deflation(c,z)
+        k1 = copy_list(c)
+        k2 = copy_list(c)
+        k2 = derivative(k2)
+        t=t+1
+#calling function
+#c = [1, -1, -7, 1, 6]
+#z = 0
+#laguerre_2(c,z,4)
+
+
 #Newton raphson method
 def newtonRaphson(q):
     t=0
@@ -440,4 +521,180 @@ def newtonRaphson(q):
     print("Root: ", q)
     print("No. of iterations:", t)
     return q
+
+#Data Fit
+
+#Interpolation
+def interpolate(a,b,x):
+    s = 0
+    l = len(a)
+    for i in range(l):
+        p = 1
+        for k in range(l):
+            if k != i:
+                p *= ((x - a[k])/(a[i]-a[k]))
+        s+=p*b[i]
+    print (x,':', s)
+    print(l,a,b)
+    return s
+    
+#Least Sq fit
+def augmentMat(z,x):
+    for i in range(0,len(z)):
+        z[i].append(x[i])
+        print(z[i])
+    return z
+
+def polySF(X,Y,degree):
+    plt.plot((X),Y)
+    plt.show()  
+    V= []
+    for i in range(0,degree):
+        a = []
+        for j in range(0,degree):
+            a.append(0)
+        V.append(a)
+    z = []
+    c = []
+    for j in range(0,2*len(V)-1):
+        sum1= 0
+        sum2 = 0
+        for i in range(0,len(X)):
+            sum1 =sum1+(X[i]**j)
+            sum2 = sum2 + Y[i]*(X[i]**(j))
+        z.append(sum1)
+        c.append(sum2)
+    c.pop()
+    c.pop()
+    for i in range(0,len(V)):
+        for j in range(0,len(V)):
+            V[i][j] = z[i+j]
+    return V,c
+#Calling fn
+#import matplotlib.pyplot as plt
+#V,c =least_sq_fit(X,Y,3)
+#augmentMat(V,c)
+#gauss_jordan(V,c)
+
+####################################################################
+#Pearson R
+def persons_ratio(x,y):
+    #for sigma =1
+    n=len(x)
+    sx,sy,sxy,sx2,sy2=0,0,0,0,0
+    for i in range(n):
+        sx+=x[i]
+        sy+=y[i]
+        sxy+=x[i]*y[i]
+        sx2+=x[i]**2
+        sy2+=y[i]**2
+    R=(n*sxy - sx*sy)/(((n*sx2 - sx*2)**0.5)*((n*sy2 - sy*2)**0.5))
+    return R
+
+#Linear Fit
+def linear_fit(X,Y):
+    # plt.scatter((X),Y)
+    # plt.show()  
+    sum_x = 0
+    sum_y = 0
+    sum_x2 = 0
+    sum_xy = 0
+    sum_y2 = 0
+    N = len(X)
+    for i in range(0,len(X)):
+        sum_x = sum_x + (X[i]/sigma[i])
+        sum_x2 = sum_x2 + ((X[i]**2)/sigma[i])
+        sum_y = sum_y + (Y[i]/sigma[i])
+        sum_xy = sum_xy + ((X[i]*Y[i])/sigma[i])
+        sum_y2 = sum_y2 + ((Y[i]**2)/sigma[i])
+    a1 = (N*sum_xy-sum_x*sum_y)/(N*sum_x2-(sum_x**2))
+    a2 = (sum_y - a1*sum_x)/(N)
+   
+    delta_x = N*sum_x2-(sum_x**2)
+    delta_y = N*sum_y2-(sum_y**2)
+    r = ((N*sum_xy - sum_x*sum_y)**2)/(delta_x*delta_y)
+    return a1,a2,r
+
+#X = [4050,4360,4920,5460,5770,6230,6910]
+#Y = [4060,4380,4910,5460,5760,6150,6870]
+#sigma = [1,1,1,1,1,1,1]
+#########################################################
+
+#Integration
+
+#Midpoint integration
+def midpoint_method(a,b,N,f):
+    h = (abs(a - b))/N
+    x = []
+    y = []
+    # print(h)
+    for i in range(1,N+1):
+        x.append((2*a + (2*i-1)*h)/2)
+       
+        y.append(f(x[i-1]))
+    # print(y)
+    # print(x)
+    sum1 = 0
+    for j in range(0,len(y)):
+        sum1 = sum1 + y[j]*h
+    print(round(sum1,9))
+ 
+#trapeziodal integration
+def trapezoidal(a,b,N,f):
+    h = (abs(a - b))/N
+    x = []
+    y = []
+    # print(h)
+   
+    for i in range(0,N+1):
+        x.append((a + i*h))
+        y.append(f(x[i]))
+    sum1 = 0
+    for i in range(1,len(x)):
+        sum1 = sum1 + (h/2)*(y[i-1]+y[i])
+    print(round(sum1,9))
+    
+#Simpsons
+def Simpsons( l, u, n ):
+ 
+    # Calculating the value of h
+    h = ( u - l )/n
+ 
+    # List for storing value of x and f(x)
+    x = list()
+    fx = list()
+    # Calculating values of x and f(x)
+    i = 0
+    while i<= n:
+        x.append(l + i * h)
+        fx.append(f(x[i]))
+        i += 1
+ 
+    # Calculating result
+    res = 0
+    i = 0
+    while i<= n:
+        if i == 0 or i == n:
+            res+= fx[i]
+        elif i % 2 != 0:
+            res+= 4 * fx[i]
+        else:
+            res+= 2 * fx[i]
+        i+= 1
+    res = round((res * (h / 3)),9)
+    return res
+
+#Finding N
+def f(x):
+    return 1/x
+
+def N_simp(l,u,t):
+    N = ((1/180*t)*24)**(1/4)
+    return N
+def N_mid(l,u,t):
+    N = ((1/24*t)*2)**(1/2)
+    return N
+def N_trap(l,u,t):
+    N = ((1/12*t)*2)**(1/2)
+    return N
 
